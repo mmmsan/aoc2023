89cefd93b843c667bbf0a63b51eafa2053fc9bbb
Marcos Santos <marcos.santos@alfaneo.ai>
2024-07-16 11:22:48 -0400

    dia 7

--- day07.py
+++ day07.py
@@ -1,7 +1,3 @@
-# relative ranking
-# tie breaker
-# multiplying total winnings
-
 def find_strength(hand: str = '23456') -> int:
     reps = []
     prev_cards = [] 
@@ -23,53 +19,61 @@ def find_strength(hand: str = '23456') -> int:
 
 
 def rank_hands(hands):
-    prev_hand = ('', 0)
-    for hand in hands:
-        if hand[1] == prev_hand[1]:
-            hand1, hand2 = tie_break(hand, prev_hand)
-            hands.remove(hand)
-            hands.remove(hand2)
-            hands.append(hand1)
-            hands.append(hand2)
-        else:
-            prev_hand = hand
+    for i in range(len(hands)):
+        if i < len(hands)-1 and hands[i][1] == hands[i+1][1]:
+            first, second = tie_break(hands[i], hands[i+1])
+            hands[i] = first
+            hands[i+1] = second
     return hands
 
 
 def tie_break(hand1, hand2):
+    card_types = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+
     if find_strength(hand1) > find_strength(hand2):
-        breakpoint()
-        hand2 = (hand2[0], int(hand2[1]) - 0.5)
+        hand2 = (hand2[0], int(hand2[1]) - 0.5, hand2[2])
+        return hand1, hand2
+    elif find_strength(hand1) < find_strength(hand2):
+        hand1 = (hand1[0], int(hand1[1]) - 0.5, hand1[2])
         return hand1, hand2
     else:
-        hand1 = (hand1[0], int(hand1[1]) - 0.5)
-        return hand1, hand2
+        for card1, card2 in zip(hand1[0], hand2[0]):
+            if card_types.index(card1) > card_types.index(card2):
+                return 
 
 
-with open('input', 'r') as f:
-    lines = f.readlines()
+def calculate_winnings(ranked_hands):
+    total = 0
+    for hand in ranked_hands:
+        idx = ranked_hands.index(hand)+1
+        sum = idx * int(hand[2])
+        total += sum
+    return total
 
 
-hands = []
-bids = []
+def main():
+    with open('input', 'r') as f:
+        lines = f.readlines()
 
-for line in lines:
-    hand, bid = line.split(' ')
-    bid = bid.replace('\n', '')
-    hands.append(hand)
-    bids.append(bid)
+    hands = []
+    bids = []
 
-powers = [] 
-for hand in hands:
-    powers.append(find_strength(hand))
+    for line in lines:
+        hand, bid = line.split(' ')
+        bid = bid.replace('\n', '')
+        hands.append(hand)
+        bids.append(bid)
 
-hands = sorted(zip(hands, powers))
-hands = rank_hands(hands)
-print(sorted(hands, key=lambda x: x[1]))
+    powers = [] 
+    for hand in hands:
+        powers.append(find_strength(hand))
+
+    hands_powers = sorted(zip(hands, powers, bids))
+    ranked_hands = rank_hands(hands_powers)
+    print(ranked_hands)
+    #winnings = calculate_winnings(ranked_hands)
 
 
+if __name__ == "__main__":
+    main()
 
-
-
-#def break_tie(hands: str):
-#def calculate_winnings(hand: str, bid: int):
